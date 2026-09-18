a717171207f7e113a13c2d54b34e6691c645166f
Pratama Ramadhan <pratamaramadhanrama@rocketmail.com>
2026-09-18 13:23:47 +0700

    chore: add lint rules

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: .center,
-          children: [
+          children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
