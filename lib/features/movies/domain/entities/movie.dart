348685eea60ba313885d6f5e99e18e37e4abed39
Pratama Ramadhan <pratamaramadhanrama@rocketmail.com>
2026-09-20 22:28:33 +0700

    fix: fix release date format

--- lib/features/movies/domain/entities/movie.dart
+++ lib/features/movies/domain/entities/movie.dart
@@ -35,6 +35,6 @@ extension MovieX on Movie {
   }
 
   String get displayReleaseDate {
-    return DateFormat('yyyy-MM-dd').format(releaseDate);
+    return DateFormat('d MMMM yyyy').format(releaseDate);
   }
 }
