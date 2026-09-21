5d61c507199d3a22bd757a96b7e61c4950a82a00
Pratama Ramadhan <pratamaramadhanrama@rocketmail.com>
2026-09-21 14:52:14 +0700

    refactor: make search bar to be more adaptive

--- lib/features/movies/presentation/widgets/custom_search_bar.dart
+++ lib/features/movies/presentation/widgets/custom_search_bar.dart
@@ -18,37 +18,21 @@ class CustomSearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: const Color(0xFFE0E0E0)),
+    return TextField(
+      controller: controller,
+      onTapOutside: (PointerDownEvent event) => FocusScope.of(context).requestFocus(FocusNode()),
+      decoration: InputDecoration(
+        hintText: hintText,
+        contentPadding: const EdgeInsets.only(bottom: kPaddingMd, top: 0),
+        hintStyle: TextStyle(color: Colors.grey[500]),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        prefixIcon: const Icon(Icons.search, color: Colors.grey),
+        suffixIcon: GestureDetector(
+          onTap: onClear,
+          child: const Icon(Icons.clear, color: Colors.grey),
+        ),
       ),
-      child: Row(
-        children: <Widget>[
-          const Icon(Icons.search, color: Colors.grey),
-          const SizedBox(width: kPaddingSm),
-          Expanded(
-            child: TextField(
-              controller: controller,
-              onTapOutside: (PointerDownEvent event) => FocusScope.of(context).requestFocus(FocusNode()),
-              decoration: InputDecoration(
-                hintText: hintText,
-                hintStyle: TextStyle(color: Colors.grey[500]),
-                border: InputBorder.none,
-                isDense: true,
-              ),
-              onChanged: onChanged,
-            ),
-          ),
-          const SizedBox(width: kPaddingSm),
-          GestureDetector(
-            onTap: onClear,
-            child: const Icon(Icons.clear, color: Colors.grey),
-          ),
-        ],
-      ),
+      onChanged: onChanged,
     );
   }
 }
